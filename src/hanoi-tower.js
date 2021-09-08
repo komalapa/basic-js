058c760e3e6f830fb4a771d1f6f9fd49863a33e2
komalapa <komalapa@gmail.com>
2021-09-08 23:24:09 +0400

    feat: hanoi towers done

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,14 @@
 import { NotImplementedError } from '../extensions/index.js';
 
+/*
+Рекурсивно решаем задачу «перенести башню из n−1 диска на 2-й штырь». Затем переносим самый большой диск на 3-й штырь, и рекурсивно решаем задачу «перенеси башню из n−1 диска на 3-й штырь».
+
+Отсюда методом математической индукции заключаем, что минимальное число ходов, необходимое для решения головоломки, равно 2^n − 1, где n — число дисков[2][3].
+
+В информатике задачи, основанные на легенде о Ханойской башне, часто рассматривают в качестве примера использования рекурсивных алгоритмов и преобразования их к нерекурсивным.
+wikipedia
+/*
+
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
@@ -14,7 +23,8 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = 2 ** disksNumber -1;
+  const output = { turns : turns, seconds: Math.floor(turns * (3600/turnsSpeed))}
+  return {...output}
 }
