d988059a83a9fd903c1a0f902ad6581a07d33948
komalapa <komalapa@gmail.com>
2021-09-16 16:36:26 +0400

    feat:extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+//options:
+// repeatTimes sets the number of repetitions of the str;
+// separator is a string separating repetitions of the str; default +
+// addition is an additional string that will be added to each repetition of the str;
+// additionRepeatTimes sets the number of repetitions of the addition;
+// additionSeparator is a string separating repetitions of the addition. default |
+  if (str === null) str = 'null';
+  let {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;
+  if (addition === null) addition = 'null';
+  let strArr = new Array(repeatTimes);
+  strArr.fill(str);
+  const additionArr = new Array(additionRepeatTimes);
+  additionArr.fill(addition);
+  const additionStr = additionArr.join(additionSeparator) 
+  strArr = strArr.map(s => s + additionStr)
+  console.log(str, options, strArr, additionArr)
+  return strArr.join(separator)
 }
